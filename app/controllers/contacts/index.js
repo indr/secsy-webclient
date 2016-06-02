43529d8b3f0fd5e1923c1b174e5ae881e902c23b
indr <mail@indr.ch>
2016-06-02 11:57:34 +0800

    Add client side filtering

--- app/controllers/contacts/index.js
+++ app/controllers/contacts/index.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
+import computedFilterByQuery from 'ember-cli-filter-by-query';
 
 export default Ember.Controller.extend({
+    // http://discuss.emberjs.com/t/filter-data-client-side/9681/2
+    // https://github.com/lazybensch/ember-cli-filter-by-query
+    filtered: computedFilterByQuery('model', ['name', 'emailAddress', 'phoneNumber'], 'query')
 });
