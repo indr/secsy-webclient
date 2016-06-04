0bbfabd2fff87cb091d69655137a0404abc33d53
indr <mail@indr.ch>
2016-06-04 12:38:35 +0800

    Add input validation

--- app/models/contact.js
+++ app/models/contact.js
@@ -1,8 +1,15 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+import { validator, buildValidations } from 'ember-cp-validations';
 
-export default Model.extend({
+const Validations = buildValidations({
+    name: validator('presence', true)
+});
+
+export default Model.extend(Validations, {
     name: attr(),
     emailAddress: attr(),
     phoneNumber: attr()
 });
+
+
