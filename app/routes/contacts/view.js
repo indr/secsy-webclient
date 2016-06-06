a25ee0a426be98c5834d1f14c881e81bcd88f884
indr <mail@indr.ch>
2016-06-06 19:52:33 +0800

    Update accessedAt via afterModel() hook

--- app/routes/contacts/view.js
+++ app/routes/contacts/view.js
@@ -4,6 +4,11 @@ export default Ember.Route.extend({
     model(params) {
         return this.store.findRecord('contact', params.id);
     },
+  
+    afterModel(model/*, transition*/) {
+      model.set('accessedAt', new Date());
+      model.save();
+    },
     
     actions: {
         delete() {
