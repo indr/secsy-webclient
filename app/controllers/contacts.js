3671688a5011b0e476749e6b2df7a02fcd809cb1
indr <mail@indr.ch>
2016-06-06 19:53:42 +0800

    Sort contacts by accessedAt

--- app/controllers/contacts.js
+++ app/controllers/contacts.js
@@ -4,5 +4,10 @@ import computedFilterByQuery from 'ember-cli-filter-by-query';
 export default Ember.Controller.extend({
     // http://discuss.emberjs.com/t/filter-data-client-side/9681/2
     // https://github.com/lazybensch/ember-cli-filter-by-query
-    filtered: computedFilterByQuery('model', ['name', 'emailAddress', 'phoneNumber'], 'query')
+  
+    sorting: ['accessedAt:desc'],
+  
+    sorted: Ember.computed.sort('model', 'sorting'),
+    
+    filtered: computedFilterByQuery('sorted', ['name', 'emailAddress', 'phoneNumber'], 'query')
 });
