1f98658b8adcd10ca4dfdf150119ee0007b10ff7
indr <mail@indr.ch>
2016-07-03 20:45:11 +0800

    Rename decrypt password to passphrase

--- app/components/decrypt-form.js
+++ app/components/decrypt-form.js
@@ -2,20 +2,20 @@ import Ember from 'ember';
 import { validator, buildValidations } from 'ember-cp-validations';
 
 const Validations = buildValidations({
-  password: validator('presence', true)
+  passphrase: validator('presence', true)
 });
 
 export default Ember.Component.extend(Validations, {
   addressbook: Ember.inject.service('addressbook'),
-  password: null,
+  passphrase: null,
 
   actions: {
     decrypt() {
       const self = this;
       const addressbook = this.get('addressbook');
-      const password = this.get('password');
+      const passphrase = this.get('passphrase');
 
-      addressbook.decrypt(password)
+      addressbook.decrypt(passphrase)
         .then(function () {
           self.sendAction('decrypted');
         });
