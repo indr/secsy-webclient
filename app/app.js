c0b2b09d0797f1f675d8908ef025a4da115d2f58
indr <mail@indr.ch>
2016-07-24 16:30:10 +0700

    Add RSVP error logging

--- app/app.js
+++ app/app.js
@@ -7,6 +7,10 @@ let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
+Ember.RSVP.on('error', function (error) {
+  Ember.Logger.assert(false, error);
+});
+
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
