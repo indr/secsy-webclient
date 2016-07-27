c6c079e946e6c24af7acefbabc5268589e733c0f
indr <mail@indr.ch>
2016-07-27 17:19:41 +0700

    Add successful /login and /logout routes

--- mirage/config.js
+++ mirage/config.js
@@ -1,28 +1,28 @@
-export default function() {
+export default function () {
 
   // These comments are here to help you get started. Feel free to delete them.
 
   /*
-    Config (with defaults).
+   Config (with defaults).
 
-    Note: these only affect routes defined *after* them!
-  */
+   Note: these only affect routes defined *after* them!
+   */
 
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
   /*
-    Shorthand cheatsheet:
+   Shorthand cheatsheet:
 
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
+   this.get('/posts');
+   this.post('/posts');
+   this.get('/posts/:id');
+   this.put('/posts/:id'); // or this.patch
+   this.del('/posts/:id');
 
-    http://www.ember-cli-mirage.com/docs/v0.2.0-beta.7/shorthands/
-  */
+   http://www.ember-cli-mirage.com/docs/v0.2.0-beta.7/shorthands/
+   */
 
   this.namespace = 'api';
   this.get('/contacts');
@@ -39,7 +39,6 @@ export default function() {
     };
   });
 
-
   // seneca-auth
   this.namespace = 'auth';
   this.post('/register', function () {
@@ -49,9 +48,18 @@ export default function() {
     };
   });
   this.post('/login', function () {
+    // return {
+    //   "ok": false,
+    //   "why": "unknown-error"
+    // };
     return {
-      "ok": false,
-      "why": "unknown-error"
+      "ok": true,
+      "login": {
+        "token": "123abc"
+      }
     };
+  });
+  this.post('/logout', function () {
+    return {};
   });
 }
--- mirage/factories/contact.js
+++ mirage/factories/contact.js
@@ -1,13 +1,33 @@
-import { Factory, faker } from 'ember-cli-mirage';
+import {Factory, faker} from 'ember-cli-mirage';
 
 export default Factory.extend({
   base64(/*i*/) {
     const data = {};
-    data['created-at'] = new Date();
-    data['accessed-at'] = new Date();
-    data['name'] = faker.name.firstName() + ' ' + faker.name.lastName();
+    data['created-at'] = now();
+    data['accessed-at'] = now();
+    data['name'] = name();
     data['email-address'] = faker.internet.email();
     data['phone-number'] = faker.phone.phoneNumber();
     return window.btoa(JSON.stringify(data));
   }
 });
+
+// export default Factory.extend({
+//   createdAt: now,
+//   accessedAt: now,
+//   name: name,
+//   emailAddress: function () {
+//     return faker.internet.email();
+//   },
+//   phoneNumber: function () {
+//     return faker.phone.phoneNumber();
+//   }
+// });
+
+function now() {
+  return new Date();
+}
+
+function name() {
+  return faker.name.firstName() + ' ' + faker.name.lastName();
+}
