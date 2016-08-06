f5d73970ba7425d0aaf2b0f10a6ddac6b8beb80d
indr <mail@indr.ch>
2016-08-06 16:09:29 +0700

    Deep encrypt/decrypt

--- app/adapters/contact.js
+++ app/adapters/contact.js
@@ -1,6 +1,3 @@
-//
-// Version: ajax() overwrite
-//
 import _ from 'lodash';
 import ApplicationAdapter from './application';
 import Ember from 'ember';
@@ -16,150 +13,98 @@ export default ApplicationAdapter.extend({
     const self = this;
     const _super = this._super;
     
-    var result;
-    if (!options || !options.data || Object.keys(options.data).length === 0) {
-      result = this._super(...arguments);
-    }
-    else {
-      result = this.encryptOne(url, type, options)
-        .then(() => _super.call(self, ...arguments));
-    }
-    return result
+    return self.deepEncrypt(options ? options.data : null)
+      .then(() => {
+        return _super.call(self, url, type, options);
+      })
       .then((response) => {
-        return self.decrypt(response);
+        return self.deepDecrypt(response);
       });
   },
   
-  decrypt(response) {
-    const data = response['contacts'] || response['contact'] || null;
-    
-    if (!data) {
-      return response;
-    }
-    
-    if (_.isArray(data)) {
-      this.decryptMany(data);
-    }
-    else {
-      this.decryptOne(data);
-    }
-    
-    return response;
-  },
-  
-  decryptMany(results) {
+  deepEncrypt(obj) {
     const self = this;
-    _.forEach(results, (each) => self.decryptOne(each));
-    return results;
-  },
-  
-  decryptOne(result) {
-    const data = result;
-    for (var key in data) {
-      if (!data.hasOwnProperty(key)) {
-        continue;
-      }
-      
-      if (key.indexOf('_') === -1) {
-        continue;
-      }
-      
-      const encrypted = data[key];
-      const plain = this.get('crypto').decrypt(encrypted);
-      Ember.assign(data, plain);
-      delete data[key];
-    }
-    
-    return result;
-  },
-  
-  encryptOne(url, type, options) {
     return new RSVP.Promise((resolve) => {
-      const data = options.data['contact'];
-      const plain = {};
-      for (var key in data) {
-        if (!data.hasOwnProperty(key)) {
-          continue;
+      const promises = [];
+      
+      if (_.isArray(obj)) {
+        for (var i = 0; i < obj.length; i++) {
+          promises.push(self.deepEncrypt(obj[i]));
+        }
+      }
+      else {
+        const plain = {};
+        
+        for (var key in obj) {
+          if (!obj.hasOwnProperty(key)) {
+            continue;
+          }
+          
+          if (key.indexOf('$') === key.length - 1) {
+            plain[key] = obj[key];
+            delete obj[key];
+            continue;
+          }
+          
+          if (_.isObject(obj[key])) {
+            promises.push(self.deepEncrypt(obj[key]));
+          }
         }
         
-        if (key.indexOf('$') === -1) {
-          continue;
+        if (Object.keys(plain).length > 0) {
+          const crypto = self.get('crypto');
+          promises.push(crypto.encryptAsync(plain)
+            .then((encrypted) => {
+              obj['encrypted_'] = encrypted;
+            }));
         }
-        
-        plain[key] = data[key];
-        delete data[key];
       }
       
-      data['encrypted_'] = this.get('crypto').encrypt(plain);
+      return RSVP.allSettled(promises)
+        .then(resolve(obj));
+    });
+  },
+  
+  deepDecrypt(obj) {
+    const self = this;
+    return new RSVP.Promise((resolve) => {
+      const promises = [];
       
-      resolve(url, type, options);
+      if (_.isArray(obj)) {
+        for (var i = 0; i < obj.length; i++) {
+          promises.push(self.deepDecrypt(obj[i]));
+        }
+      }
+      else {
+        var encrypted = null;
+        
+        for (var key in obj) {
+          if (!obj.hasOwnProperty(key)) {
+            continue;
+          }
+          
+          if (key === 'encrypted_') {
+            encrypted = obj[key];
+            delete obj[key];
+            continue;
+          }
+          
+          if (_.isObject(obj[key])) {
+            promises.push(self.deepDecrypt(obj[key]));
+          }
+        }
+        
+        if (encrypted !== null) {
+          const crypto = this.get('crypto');
+          promises.push(crypto.decryptAsync(encrypted)
+            .then((plain) => {
+              Ember.assign(obj, plain);
+            }));
+        }
+      }
+      
+      return RSVP.allSettled(promises)
+        .then(resolve(obj));
     });
   }
 });
-
-//
-// Version: ApplicationAdapter as this.inner
-//
-// import Ember from 'ember';
-// import Adapter from "ember-data/adapter";
-//
-// const {
-//   RSVP
-// } = Ember;
-//
-// export default Adapter.extend({
-//   inner: null,
-//
-//   init() {
-//     this._super(...arguments);
-//     const applicationInstance = Ember.getOwner(this);
-//     this.inner = applicationInstance.lookup('adapter:application');
-//   },
-//
-//   createRecord() {
-//     return this.inner.createRecord(...arguments);
-//   },
-//
-//   deleteRecord() {
-//     return this.inner.deleteRecord(...arguments);
-//   },
-//
-//   findAll() {
-//     return this.inner.findAll(...arguments)
-//       .then(this.decryptMany);
-//   },
-//
-//   findMany() {
-//     return this.inner.findMany(...arguments)
-//       .then(this.decryptMany);
-//   },
-//
-//   findRecord() {
-//     return this.inner.findRecord(...arguments)
-//       .then(this.decryptOne);
-//   },
-//
-//   updateRecord(store, type, snapshot) {
-//     return this.encryptOne(store, type, snapshot)
-//       .then(() => {
-//         return this.inner.updateRecord(...arguments);
-//       });
-//   },
-//
-//   decryptMany(results) {
-//     console.log('decryptMany', results);
-//     return results;
-//   },
-//
-//   decryptOne(result) {
-//     console.log('decryptOne', result);
-//     return result;
-//   },
-//
-//   encryptOne(store, type, snapshot) {
-//     console.log('encryptOne', snapshot);
-//     return new RSVP.Promise((resolve, reject) => {
-//       resolve(store, type, snapshot);
-//     })
-//   }
-// });
