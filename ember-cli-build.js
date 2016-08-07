1e05214b0052ae831575da5e437eabe74edb6897
indr <mail@indr.ch>
2016-08-07 08:08:32 +0700

    Add bootstrap.min.js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -22,6 +22,7 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
   app.import('bower_components/openpgp/dist/openpgp.min.js');
   app.import('vendor/shims/openpgp.js', {
     exports: {
