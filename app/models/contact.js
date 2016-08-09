89b08e1eb55bc42351757b89f9ee0e5ed284cbfb
indr <mail@indr.ch>
2016-08-09 12:12:17 +0700

    Add computed property location

--- app/models/contact.js
+++ app/models/contact.js
@@ -1,23 +1,35 @@
-import Model from 'ember-data/model';
+import Ember from 'ember';
 import attr from 'ember-data/attr';
+import Model from 'ember-data/model';
 import { validator, buildValidations } from 'ember-cp-validations';
 
 const Validations = buildValidations({
-    name$: validator('presence', true)
+  name$: validator('presence', true)
 });
 
 export default Model.extend(Validations, {
-    createdAt: attr('date', {
-      defaultValue() { return new Date(); }
-    }),
-    accessedAt: attr('date', {
-      defaultValue() { return new Date(); }
-    }),
-    name$: attr(),
-    emailAddress$: attr(),
-    phoneNumber$: attr(),
-    latitude$: attr(),
-    longitude$: attr()
+  createdAt: attr('date', {
+    defaultValue() {
+      return new Date();
+    }
+  }),
+  accessedAt: attr('date', {
+    defaultValue() {
+      return new Date();
+    }
+  }),
+  name$: attr(),
+  emailAddress$: attr(),
+  phoneNumber$: attr(),
+  latitude$: attr(),
+  longitude$: attr(),
+  
+  location: Ember.computed('latitude$', 'longitude$', function () {
+    if (!this.get('latitude$') || !this.get('longitude$')) {
+      return null;
+    }
+    return [this.get('latitude$'), this.get('longitude$')];
+  })
 });
 
 
--- tests/unit/models/contact-test.js
+++ tests/unit/models/contact-test.js
@@ -1,17 +1,29 @@
 import { assert } from 'chai';
-import {describeModel, it } from 'ember-mocha';
+import { describeModel, it } from 'ember-mocha';
 
 describeModel('contact', 'Unit | Model | contact', {
-  // Specify the other units that are required for this test.
-  needs: [
-    'validator:presence'
-  ]
+    // Specify the other units that are required for this test.
+    needs: [
+      'validator:presence'
+    ]
   },
   function () {
-
-it('it exists', function () {
-  let model = this.subject();
-  // let store = this.store();
-  assert.ok(!!model);
-});
-});
+    
+    it('it exists', function () {
+      let model = this.subject();
+      // let store = this.store();
+      assert.ok(!!model);
+    });
+    
+    it('location', function () {
+      let sut = this.subject();
+      assert.deepEqual(sut.get('location'), null);
+      sut.set('latitude$', 45);
+      assert.deepEqual(sut.get('location'), null);
+      sut.set('longitude$', 105);
+      assert.deepEqual(sut.get('location'), [45, 105]);
+      sut.set('latitude$', null);
+      assert.deepEqual(sut.get('location'), null);
+    });
+  }
+);
