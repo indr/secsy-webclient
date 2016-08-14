0427257ba55f30be74b21a3c400c3a4ef4061c20
indr <mail@indr.ch>
2016-08-14 09:47:37 +0700

    Add RESTful serializer for sails back-end

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,3 +1,38 @@
 import DS from 'ember-data';
 
-export default DS.RESTSerializer.extend({});
+export default DS.RESTSerializer.extend({
+  /**
+   *  Embeds the payload in a root object.
+   */
+  _normalizeResponse(store, primaryModelClass, payload, id, requestType, isSingle) {
+    const modelName = primaryModelClass.modelName;
+    const transformed = {};
+    transformed[modelName] = payload;
+    
+    return this._super(store, primaryModelClass, transformed, id, requestType, isSingle);
+  },
+  
+  /**
+   * Extracts the root object of hash.
+   */
+  serializeIntoHash(hash, typeClass, snapshot, options) {
+    const result = this._super(hash, typeClass, snapshot, options);
+    
+    if (Object.keys(hash).length > 1) {
+      throw new Error('Can not serialize more than one object');
+    }
+    
+    const rootKey = Object.keys(hash)[0];
+    const root = hash[rootKey];
+    var rootKeyOverwritten = false;
+    Object.keys(root).forEach((eachKey) => {
+      hash[eachKey] = root[eachKey];
+      rootKeyOverwritten = rootKeyOverwritten || eachKey === rootKey;
+    });
+    if (!rootKeyOverwritten) {
+      delete hash[rootKey];
+    }
+    
+    return result;
+  }
+});
