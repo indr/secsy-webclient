7091b6d80c61357cb7020546e2c5e40d49a44345
indr <mail@indr.ch>
2016-08-19 08:56:36 +0700

    Reopen validator ds-error for translation and handling of DS.Error

---
+++ app/validators/ds-error.js
@@ -0,0 +1,29 @@
+import DS from 'ember-data';
+import DSError from 'ember-cp-validations/validators/ds-error';
+import Ember from 'ember';
+
+const {get, isNone} = Ember;
+
+export default DSError;
+
+DSError.reopen({
+  validate(value, options, model, attribute) {
+    const errors = get(model, 'errors');
+    
+    if (!isNone(errors) && errors instanceof DS.Errors && errors.has(attribute)) {
+      
+      var result = get(errors.errorsFor(attribute), 'lastObject.message');
+      
+      // Let ember-intl-cp-validations translate the error message
+      result = this.createErrorMessage(result, value, options);
+      
+      // Remove the error from the collection so that during the next validation, that is when the user
+      // changed the value, the validation succeeds again, and, thus can be validated against the server again.
+      errors._remove(attribute);
+      
+      return result;
+    }
+    
+    return true;
+  }
+});
