16381780ab2947964efc15000ce6ad1f533e660d
indr <mail@indr.ch>
2016-08-19 08:57:12 +0700

    Add initializer to import reopened classes

---
+++ app/initializers/reopen-classes.js
@@ -0,0 +1,11 @@
+/*jshint unused:false*/
+import DSError from './../validators/ds-error';
+
+export function initialize(/* application */) {
+  // application.inject('route', 'foo', 'service:foo');
+}
+
+export default {
+  name: 'reopen-classes',
+  initialize
+};
