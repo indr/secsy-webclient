d09c52f48e1c496042022fb9a6693a2688961f15
indr <mail@indr.ch>
2016-08-19 08:50:40 +0700

    Normalize error responses from sails backend to be consumed by DS.Errors

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -15,5 +15,33 @@ export default DS.RESTAdapter.extend({
       headers[key] = token;
       return headers;
     }
-  })
+  }),
+  
+  normalizeErrorResponse(status, headers, payload) {
+    const errors = [];
+    
+    if (payload && typeof payload === 'object') {
+      if (payload.invalidAttributes) {
+        Object.keys(payload.invalidAttributes).forEach(function (eachKey) {
+          const pointer = 'data/attributes/' + eachKey;
+          payload.invalidAttributes[eachKey].forEach(function (obj) {
+            errors.push({
+              detail: obj.message,
+              source: {pointer}
+            });
+          });
+        });
+      }
+    }
+    
+    if (errors.length === 0 || errors.message) {
+      errors.push({
+        status: `${status}`,
+        title: payload.message || 'The backend responded with an error',
+        detail: `${payload}`
+      });
+    }
+    console.log('errors', errors);
+    return errors;
+  }
 });
