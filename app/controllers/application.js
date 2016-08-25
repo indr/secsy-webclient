35efd75ccbc56f84ad148393bb089683e6bf96c9
indr <mail@indr.ch>
2016-08-25 17:23:19 +0700

    Disable no ssl warning for localhost

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
   
-  noSsl: window.location.href.indexOf('https') !== 0,
+  noSsl: window.location.href.indexOf('https') !== 0 && window.location.href.indexOf('http://localhost') !== 0,
   
   isModal: false,
   progress: {value: 0, max: 0, type: 'info'}
