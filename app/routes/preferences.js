82092f6af47f9e1f4f1280659e05132a1db9109e
indr <mail@indr.ch>
2016-08-25 16:06:48 +0700

    Move actions from preferences to navbar

--- app/routes/preferences.js
+++ app/routes/preferences.js
@@ -1,7 +1,8 @@
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import DecryptedRouteMix from './../mixins/decrypted-route-mixin';
 import Ember from 'ember';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+export default Ember.Route.extend(AuthenticatedRouteMixin, DecryptedRouteMix, {
   addressbook: Ember.inject.service(),
   
   actions: {
