0f96793c42d76aec21d8a00a65268d157e5220f0
indr <mail@indr.ch>
2016-09-16 01:04:36 +0700

    Add password confirmation

---
+++ app/components/delete-account-form.js
@@ -0,0 +1,41 @@
+import Ember from 'ember';
+import { validator, buildValidations } from 'ember-cp-validations';
+import ValidationErrorsMixin from '../mixins/validation-errors-mixin';
+
+const Validations = buildValidations({
+  password: {
+    validators: [
+      validator('presence', true),
+      validator('length', {min: 8, max: 64}),
+      validator('ds-error')
+    ]
+  }
+});
+
+export default Ember.Component.extend(Validations, ValidationErrorsMixin, {
+  ajax: Ember.inject.service(),
+  session: Ember.inject.service(),
+  
+  message: null,
+  password: null,
+  
+  actions: {
+    deleteAccount() {
+      const flash = this.get('flashMessages');
+      const {message, password} = this.getProperties('message', 'password');
+      
+      flash.clearMessages();
+      
+      this.get('ajax').delete('/api/users/me', {message, password}).then(() => {
+        flash.successT('profile.delete-account.success');
+        // Ember.run.later(() => {
+        //   this.get('session').invalidate();
+        // }, 1500);
+      }).catch((error) => {
+        return this.handleValidationErrors(error);
+      }).catch((error) => {
+        flash.dangerT('profile.delete-account.unknown-error', error.getMessage() || error, {sticky: false});
+      });
+    }
+  }
+});
--- app/routes/preferences/delete-account.js
+++ app/routes/preferences/delete-account.js
@@ -1,24 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
-  ajax: Ember.inject.service(),
-  session: Ember.inject.service(),
-  
-  actions: {
-    deleteAccount() {
-      const flash = this.get('flashMessages');
-      const message = this.controller.get('deleteMessage');
-      
-      flash.clearMessages();
-      
-      this.get('ajax').delete('/api/users/me', {message}).then(() => {
-        flash.successT('profile.delete-account.success');
-        Ember.run.later(() => {
-          this.get('session').invalidate();
-        }, 1500);
-      }).catch((error) => {
-        flash.dangerT('profile.delete-account.unknown-error', error.getMessage() || error, {sticky: false});
-      });
-    }
-  }
-});
+export default Ember.Route.extend({});
