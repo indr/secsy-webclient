c91769ab682a57332307987e6662d13acd78e2d1
indr <mail@indr.ch>
2016-09-15 16:57:55 +0700

    Send password reset email

--- app/routes/preferences.js
+++ app/routes/preferences.js
@@ -4,14 +4,33 @@ import Ember from 'ember';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, DecryptedRouteMix, {
   addressbook: Ember.inject.service(),
+  ajax: Ember.inject.service(),
+  session: Ember.inject.service(),
+  
+  model() {
+    const email = this.get('session').get('data.authenticated.email');
+    return Ember.Object.create({
+      email
+    });
+  },
   
   actions: {
+    
     deleteAccount() {
       console.log('delete account');
     },
     
     sendPasswordResetEmail() {
-      console.log('send password reset email');
+      const flash = this.get('flashMessages');
+      const email = this.controller.get('model.email');
+      
+      flash.clearMessages();
+      
+      this.get('ajax').post('/api/users/forgot-password', {email}).then(() => {
+        flash.successT('profile.reset-email.success');
+      }).catch((error) => {
+        flash.dangerT('profile.reset-email.unknown-error', error.getMessage() || error, {sticky: false});
+      });
     },
     
     generateFakes() {
