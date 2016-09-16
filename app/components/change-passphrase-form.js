dd51053fe0c43343a49ff4494b654fdb07d5fb5e
indr <mail@indr.ch>
2016-09-17 00:36:33 +0700

    Implement change-passphrase-form

--- app/components/change-passphrase-form.js
+++ app/components/change-passphrase-form.js
@@ -1,4 +1,43 @@
 import Ember from 'ember';
+import { validator, buildValidations } from 'ember-cp-validations';
+import ValidationErrorsMixin from '../mixins/validation-errors-mixin';
 
-export default Ember.Component.extend({
+const Validations = buildValidations({
+  password: {
+    validators: [
+      validator('presence', true),
+      validator('length', {min: 8, max: 64}),
+      validator('ds-error')
+    ]
+  },
+  passphrase: {
+    validators: [
+      validator('presence', true),
+      validator('length', {min: 8, max: 64})
+    ]
+  },
+  newPassphrase: {
+    validators: [
+      validator('presence', true),
+      validator('length', {min: 8, max: 64})
+    ]
+  },
+  confirmPassphrase: {
+    validators: [
+      validator('presence', true),
+      validator('confirmation', {
+        on: 'newPassphrase', messageKey: 'errors.confirmation-new-passphrase'
+      })
+    ]
+  }
+});
+
+export default Ember.Component.extend(Validations, ValidationErrorsMixin, {
+  actions: {
+    changePassphrase() {
+      const flash = this.get('flashMessages');
+      
+      flash.dangerT('profile.change-passphrase.unknown-error');
+    }
+  }
 });
