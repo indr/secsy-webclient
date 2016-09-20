31a26f387fd8bc2e04d8dc5748323e8250c6547d
indr <mail@indr.ch>
2016-09-20 13:49:17 +0700

    Show share button and updates available based on preference

--- app/components/contact-view.js
+++ app/components/contact-view.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  session: Ember.inject.service(),
+  
+  canShare: Ember.computed('session.data.authenticated.sync_enabled', function () {
+    return this.get('session').get('data.authenticated.sync_enabled');
+  }),
+  
   actions: {
     delete(model) {
       this.sendAction('delete', model);
--- app/controllers/preferences/index.js
+++ app/controllers/preferences/index.js
@@ -1,9 +1,9 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+export default Ember.Controller.extend({
   session: Ember.inject.service(),
   
-  canReview: Ember.computed('session.data.authenticated.sync_enabled', function () {
+  canPullUpdates: Ember.computed('session.data.authenticated.sync_enabled', function () {
     return this.get('session').get('data.authenticated.sync_enabled');
   })
 });
