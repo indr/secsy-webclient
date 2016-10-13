4ec55a47df990a7536f76de70434c9bcb4319a67
Reto Inderbitzin <mail@indr.ch>
2016-10-13 22:59:42 +0800

    Fix leaflet marker images (#6)
    * Exclude leaflet images from fingerprinting
    
    * Update ember-leaflet@3.0.1

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -16,6 +16,15 @@ module.exports = function (defaults) {
     'ember-bootstrap': {
       'importBootstrapTheme': false,
       'importBootstrapCSS': false
+    },
+    fingerprint: {
+      exclude: [
+        'images/layers-2x.png',
+        'images/layers.png',
+        'images/marker-icon-2x.png',
+        'images/marker-icon.png',
+        'images/marker-shadow.png'
+      ]
     }
   });
   
